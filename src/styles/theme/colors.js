54ff5a107efa6063cc2a68a6fa1d07bd4a7bad3c
jin <qktjwl123@gmail.com>
2025-03-15 15:15:01 +0900

    chore: 디자인 시스템 구축 및 적용

---
+++ src/styles/theme/colors.js
@@ -0,0 +1,43 @@
+const colors = {
+
+    BlackAndWhite:
+    {
+        black: '#000000',
+        white: '#ffffff',
+    },
+    Gray: {
+        gray50: '#f7fafc',
+        gray100: '#edf2f7',
+        gray200: '#e2e8f0',
+        gray300: '#cbd5e0',
+        gray400: '#a0aec0',
+        gray500: '#718096',
+        gray600: '#4a5568',
+        gray700: '#2d3748',
+        gray800: '#1a202c',
+        gray900: '#171923',
+    },
+    Alpha: {
+        alpha: '#000000',
+    },
+    WBOrange: {
+        wbo50: '#ffedec',
+        wbo100: '#ffded9',
+        wbo200: '#ffc1b6',
+        wbo300: '#ff9584',
+        wbo400: '#f97561',
+        wbo500: '#ff5c33',
+        wbo600: '#ce4c2d',
+        wbo700: '#9a402a',
+        wbo800: '#6f3527',
+        wbo900: '#411a10',
+    },
+    AlertColor: {
+        blue500: '#007bff',
+        yellow400: '#ffc107',
+        red500: '#dc3545',
+        green400: '#28a745',
+    }
+};
+
+export default colors;
