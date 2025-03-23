320b7aa7ccdf363f504cef23edef26e623ea12b6
jin <qktjwl123@gmail.com>
2025-03-23 20:03:44 +0900

    chore: ts환경 설정으로 변경

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import App from './App';
 
 import { ThemeProvider } from 'styled-components';
-import theme from '@styles/theme/theme.js';
+import theme from '@styles/theme/theme';
 
 import '@styles/global/global.scss';
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <App />
