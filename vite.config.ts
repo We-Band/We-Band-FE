e59c12d98998875e6d1865227adcfd8206effe7f
jin <qktjwl123@gmail.com>
2025-03-25 17:48:28 +0900

    chore: svg 사용을 위한 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import svgr from 'vite-plugin-svgr';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,5 +23,5 @@ export default defineConfig({
       '@styles': path.resolve(__dirname, 'src/styles'),
     },
   },
-  plugins: [react()],
+  plugins: [react(), svgr()],
 });
