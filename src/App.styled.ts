2209e108c88fa61ada420f00c45509400461fab3
jin <qktjwl123@gmail.com>
2025-03-27 23:02:21 +0900

    fix: 고정형 웹으로 변경

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const AppContainer = styled.div`
-  max-width: 450px;
-  min-width: 350px;
+  width: 400px;
   margin: 0 auto;
 `;
--- src/layout/main/MainLayout.styled.ts
+++ src/layout/main/MainLayout.styled.ts
@@ -6,4 +6,5 @@ export const LogoContainer = styled.div`
   display: flex;
   align-items: center;
   height: 80px;
+  padding-left: 30px;
 `;
