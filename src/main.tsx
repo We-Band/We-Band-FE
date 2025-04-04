9b3351b138e562b067ad76f91416254b1a03e345
jin <qktjwl123@gmail.com>
2025-04-04 23:33:23 +0900

    feat: Web Analytics 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from './App';
 
 import { ThemeProvider } from 'styled-components';
 import theme from '@styles/theme/theme';
+import { Analytics } from '@vercel/analytics/react';
 
 import '@styles/global/global.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <Analytics />
       <App />
     </ThemeProvider>
   </StrictMode>,
