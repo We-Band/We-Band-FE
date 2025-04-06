acaaa847a4e8dc30ecaed7a78897800bab2a2d47
jin <qktjwl123@gmail.com>
2025-04-06 09:36:43 +0900

    hotfix: 디버깅용 콘솔출력 제거

--- src/components/calendar/LiteCalendar.tsx
+++ src/components/calendar/LiteCalendar.tsx
@@ -15,8 +15,6 @@ const LiteCalendar = ({ userToggle }: { userToggle: boolean }) => {
   const { selectedUser } = useLiteContext();
   const [selectedUserSchedule, setSelectedUserSchedule] = useState('');
 
-  console.log('userData', userData);
-
   useEffect(() => {
     if (selectedUser === '') {
       setSelectedUserSchedule(NOSCHEDULE);
