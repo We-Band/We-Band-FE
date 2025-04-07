7ba162e752414301002b5b32ffe157773e489a16
jin <qktjwl123@gmail.com>
2025-04-08 01:52:43 +0900

    feat: html에 에드센스 코드 추가

---
+++ src/components/AdBanner/AdBanner.tsx
@@ -0,0 +1,30 @@
+// import { useEffect } from 'react';
+
+// declare global {
+//   interface Window {
+//     adsbygoogle: any[];
+//   }
+// }
+
+// function AdBanner() {
+//   useEffect(() => {
+//     try {
+//       (window.adsbygoogle = window.adsbygoogle || []).push({});
+//     } catch (e) {
+//       console.error('Adsense error', e);
+//     }
+//   }, []);
+
+//   return (
+//     <ins
+//       className="adsbygoogle"
+//       style={{ display: 'block' }}
+//       data-ad-client="ca-pub-XXXXXXXXXXXX"
+//       data-ad-slot="YYYYYYYYYYYY"
+//       data-ad-format="auto"
+//       data-full-width-responsive="true"
+//     ></ins>
+//   );
+// }
+
+// export default AdBanner;
