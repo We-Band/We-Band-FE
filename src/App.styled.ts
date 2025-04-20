c079eb97176774e7c2ee3d08e498c99be3f17fdc
박성진 <qktjwl123@gmail.com>
2025-04-20 23:57:02 +0900

    feat: lite footer 구현 (#27)
    * feat: lite footer 생성
    
    * fix: lite footer에 home 연결

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,7 +1,3 @@
 import styled from 'styled-components';
 
-export const AppContainer = styled.div`
-  width: 100%;
-  max-width: 400px;
-  margin: 0 auto;
-`;
+export const AppContainer = styled.div``;
