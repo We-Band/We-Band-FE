9b07a605761bd171acdd0273f2be4526640f65c9
jin <qktjwl123@gmail.com>
2025-04-20 23:42:44 +0900

    Revert "feat: lite footer 생성"
    This reverts commit 7d80787bd00a78ce1b07374405640392a59db548.

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 
 export const AppContainer = styled.div`
   width: 100%;
+  max-width: 400px;
   margin: 0 auto;
 `;
--- src/layout/lite/LiteLayout.styled.ts
+++ src/layout/lite/LiteLayout.styled.ts
@@ -1,52 +1,10 @@
 import styled from 'styled-components';
 
-export const LiteLayoutContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-`;
+export const LiteLayoutContainer = styled.div``;
 
 export const LogoContainer = styled.div`
   display: flex;
-  width: 400px;
-  height: 80px;
   align-items: center;
-  align-self: center;
-
+  height: 80px;
   padding-left: 32px;
-`;
-
-export const MainContentContainer = styled.main`
-  flex: 1;
-  width: 400px;
-  align-self: center;
-`;
-
-export const FooterBackground = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  background-color: ${({ theme }) => theme.colors.Gray.gray200};
-`;
-
-export const FooterContainer = styled.div`
-  display: flex;
-  height: 120px;
-  padding: 28px 36px;
-  justify-content: space-between;
-  align-items: flex-start;
-  width: 400px;
-`;
-
-export const FooterLinkContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: flex-end;
-  gap: 4px;
-`;
-
-export const FooterLink = styled.a`
-  ${({ theme }) => theme.fontStyles.S01_Medium};
-  color: ${({ theme }) => theme.colors.Gray.gray500};
 `;
