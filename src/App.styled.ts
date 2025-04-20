7d80787bd00a78ce1b07374405640392a59db548
jin <qktjwl123@gmail.com>
2025-04-20 23:41:13 +0900

    feat: lite footer 생성

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -2,6 +2,5 @@ import styled from 'styled-components';
 
 export const AppContainer = styled.div`
   width: 100%;
-  max-width: 400px;
   margin: 0 auto;
 `;
--- src/layout/lite/LiteLayout.tsx
+++ src/layout/lite/LiteLayout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router';
 import * as S from './LiteLayout.styled';
 import { useNavigate } from 'react-router-dom';
 import LiteLogo from '@assets/logo/lite-logo.svg?react';
+import FooterLogo from '@assets/logo/footer-logo.svg?react';
 
 const LiteLayout = () => {
   const navigate = useNavigate();
@@ -15,7 +16,30 @@ const LiteLayout = () => {
       <S.LogoContainer>
         <LiteLogo onClick={handleClick} />
       </S.LogoContainer>
-      <Outlet />
+
+      <S.MainContentContainer>
+        <Outlet />
+      </S.MainContentContainer>
+
+      <S.FooterBackground>
+        <S.FooterContainer>
+          <FooterLogo />
+          <S.FooterLinkContainer>
+            <S.FooterLink
+              href="https://glacier-borogovia-7b5.notion.site/We-Band-Lite-1ccdf3d703f28039b961c72d95d691d3"
+              target="_blank"
+            >
+              WeBand 더 알아보기
+            </S.FooterLink>
+            <S.FooterLink
+              href="mailto:qktjwl123@gmail.com?subject=WeBand 관련 문의드립니다."
+              target="_blank"
+            >
+              문의 및 버그제보
+            </S.FooterLink>
+          </S.FooterLinkContainer>
+        </S.FooterContainer>
+      </S.FooterBackground>
     </S.LiteLayoutContainer>
   );
 };
