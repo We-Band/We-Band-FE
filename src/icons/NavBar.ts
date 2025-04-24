6c62acad89146542408a2a4691907e86a46758f7
박성진 <qktjwl123@gmail.com>
2025-04-25 06:13:44 +0900

    feat: navbar 구현 (#34)

---
+++ src/icons/NavBar.ts
@@ -0,0 +1,14 @@
+import ClubOn from '@assets/icons/club-on.svg?react';
+import ClubOff from '@assets/icons/club-off.svg?react';
+import CalendarOn from '@assets/icons/calendar-on.svg?react';
+import CalendarOff from '@assets/icons/calendar-off.svg?react';
+import MyOn from '@assets/icons/my-on.svg?react';
+import MyOff from '@assets/icons/my-off.svg?react';
+import styled from 'styled-components';
+
+export const ClubOnIcon = styled(ClubOn)``;
+export const ClubOffIcon = styled(ClubOff)``;
+export const CalendarOnIcon = styled(CalendarOn)``;
+export const CalendarOffIcon = styled(CalendarOff)``;
+export const MyOnIcon = styled(MyOn)``;
+export const MyOffIcon = styled(MyOff)``;
---
+++ src/layout/main/navBar/NavBar.styled.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const NavBarContainer = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 0px 37px 28px 37px;
+  justify-content: center;
+  justify-content: space-between;
+  position: fixed;
+  bottom: 0px;
+
+  border-radius: 16px 16px 0px 0px;
+  background: ${({ theme }) => theme.colors.Gray.gray100};
+`;
