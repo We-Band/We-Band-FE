617312542236d4fc2c2f7c5a9124edd7cd53a789
jin <qktjwl123@gmail.com>
2025-04-26 19:08:03 +0900

    feat: 로그인 실패시 처리

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -2,7 +2,7 @@ import { postKakaoLogin } from '@api/auth';
 import { OnboardingLogo } from '@icons/Logo';
 import * as S from './Auth.styled';
 import { useAuthStore } from '@store/auth';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { LoadingOnboarding } from '@icons/Loading';
 
@@ -10,6 +10,7 @@ const Auth = () => {
   const login = useAuthStore((state) => state.login);
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+  const [loginFail, setLoginFail] = useState(false);
 
   useEffect(() => {
     const code = searchParams.get('code');
@@ -25,17 +26,35 @@ const Auth = () => {
           }
         })
         .catch((err) => {
+          setLoginFail(true);
           console.error(err);
         });
     }
   }, [searchParams, navigate]);
 
+  useEffect(() => {
+    if (loginFail) {
+      setTimeout(() => {
+        navigate('/onboarding');
+      }, 5000);
+    }
+  }, [loginFail, navigate]);
+
   return (
     <S.AuthContainer>
       <S.AuthSection>
         <OnboardingLogo />
-        <LoadingOnboarding />
-        <S.AuthText>로그인 중입니다...</S.AuthText>
+
+        {loginFail ? (
+          <S.AuthText>
+            로그인에 실패했습니다. <br /> 잠시 후 로그인 페이지로 넘어갑니다.
+          </S.AuthText>
+        ) : (
+          <>
+            <LoadingOnboarding />
+            <S.AuthText>로그인 중입니다...</S.AuthText>
+          </>
+        )}
       </S.AuthSection>
     </S.AuthContainer>
   );
